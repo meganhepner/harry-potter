3b420c98fd5c517872b4619830220878903f14bc
Megan Hepner <megan.hepner@gmail.com>
2020-07-01 11:54:33 -0700

    create spell search returning spell name and effect
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-Authored-By: chris yoon <chy.yoon@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,10 +3,34 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+function getElements(response, spell) {
+  if (response.find(element => element.spell === spell)) {
+    let responseSpell = response.find(element => element.spell === spell);
+    $('.castSpell').text(`You have cast ${spell}, which ${responseSpell.effect}`)
+  } else {
+    $('castSpell').text("");
+    $('.showErrors').text(`There was an error: ${response.message}`)
+  }
+  
+}
+
 $(document).ready(function () {
   $('#cast').click(function() {
       const spell = $('#spell').val();
       $('#spell').val("");
-  });
+      fetch(`https://www.potterapi.com/v1/spells?key=${process.env.API_KEY}`)
+        .then(function(response) {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          return response.json();
+        })
+        .catch(function(error) {
+          return error;
+        })
+        .then(function(jsonifiedResponse){
+          getElements(jsonifiedResponse, spell);
+        });
   
   });
+});
